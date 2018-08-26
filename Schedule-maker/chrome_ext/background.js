94ad6e200bc18c56dcb9b76f0777c444572ee54f
Siddhant Pagariya <siddhant.pagariya@gmail.com>
2018-08-25 23:27:25 -0400

    Added icons and in progress with JS and directory restructuring

--- Schedule-maker/chrome_ext/background.js
+++
@@ -1,8 +0,0 @@
-chrome.runtime.onInstalled.addListener(function() {
-    /*chrome.storage.sync.set({color: '#3aa757'}, function() {
-      console.log("The color is green.");
-  });*/
-  });
-if (document.getElementById('ptifrmtgtframe').contentDocument.getElementById('STDNT_WEEK_SCHD$scroll$0')){
-    console.log("Yes!");
-}
