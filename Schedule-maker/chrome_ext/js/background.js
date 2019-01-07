521b65eda98bc928f67fb6a0857556ef6e24a482
Siddhant Pagariya <siddhant.pagariya@gmail.com>
2019-01-07 02:44:46 -0500

    Updated Chrome Extension Beta

--- Schedule-maker/chrome_ext/js/background.js
+++ Schedule-maker/chrome_ext/js/background.js
@@ -20,8 +20,12 @@ RRULE:FREQ=YEARLY;BYMONTH=3;BYDAY=2SU
 END:DAYLIGHT
 END:VTIMEZONE
 `;
+
+function weekMod(num){
+    return num%7;
+}
 function createICal(jsonObj){
-    //console.log(jsonObj);
+    console.log(jsonObj);
     window.json = jsonObj;
     let vtimezoneComp = new ICAL.Component(ICAL.parse(vtimezone_str));
     let tzid = vtimezoneComp.getFirstPropertyValue('tzid');
@@ -31,55 +35,68 @@ function createICal(jsonObj){
     });
     let cal = new ICAL.Component(['vcalendar', [["prodid", {}, "text", "-//SidPagariya.ML//UMich "+jsonObj['term']+"//EN"],
     ["version", {}, "text", "2.0"],
-    ["X-Apple-Calendar-Color", {}, "text", "#FFCD00"]], []]); //Or maybe just #002B64 :D
+    ["X-Apple-Calendar-Color", {}, "text", "#FFCD00"]], []]); //Or maybe just #002B64 :P
     arbor_time = 'America/Detroit';
     cal.updatePropertyWithValue('X-WR-TIMEZONE', arbor_time);
     cal.addSubcomponent(vtimezoneComp);
-    var start_date = new Date(2018, 8, 4);
-    var end_date = new Date(2018, 11, 12);
+
+    // Need to change this every term :(
+    var start_date = new Date(2019, 0, 9); //YEAR, MONTH, DAY. MONTH is 0...11
+    var end_date = new Date(2019, 3, 23); //YEAR, MONTH, DAY. MONTH is 0...11
 
     for(course of jsonObj.schedule) {
         let classTitle = course.course.class;
         let classType = course.course.type;
         let classSec = course.course.sec;
         let classNbr = course.course.nbr;
-        let byday = course.sched.days;
-        let start_time = course.sched.time.from;
-        let end_time = course.sched.time.to;
-        let conversionO = {MO:6,TU:0,WE:1,TH:2,FR:3,SA:4,SU:5};
-        var minDiff = 10;
-        for (day of byday){
-            if (conversionO[day] < minDiff){
-                minDiff = conversionO[day];
+
+        for (var i=0; i<course.sched.length; i++){
+            let byday = course.sched[i].days;
+            if (!course.sched[i].time){
+                continue;
             }
-        }
-        let dtstart = new Date(start_date.getUTCFullYear(), start_date.getUTCMonth(), start_date.getUTCDate()+minDiff, start_time.hr, start_time.min, 0);
-        let dtend = new Date(start_date.getUTCFullYear(), start_date.getUTCMonth(), start_date.getUTCDate()+minDiff, end_time.hr, end_time.min, 0);
-        let location = course['location'];
-        String.prototype.newFormat = function() {
-            var args = arguments;
-            return this.replace(/{(\d+)}/g, function(match, number) {
-                return typeof args[number] != 'undefined'
-                ? args[number]
-                : match
-                ;
+            let start_time = course.sched[i].time.from;
+            let end_time = course.sched[i].time.to;
+
+            // Need to change this every term :(
+            let offset = 5; //Schedules starting on Monday: 7, Tuesday: 6, etc.
+            //Winter 2019 ^
+
+            let conversionO = {MO:weekMod(0+offset),TU:weekMod(1+offset),WE:weekMod(2+offset),TH:weekMod(3+offset),FR:weekMod(4+offset),SA:weekMod(5+offset),SU:weekMod(6+offset)};
+            var minDiff = 10;
+            for (day of byday){
+                if (conversionO[day] < minDiff){
+                    minDiff = conversionO[day];
+                }
+            }
+            let dtstart = new Date(start_date.getUTCFullYear(), start_date.getUTCMonth(), start_date.getUTCDate()+minDiff, start_time.hr, start_time.min, 0);
+            let dtend = new Date(start_date.getUTCFullYear(), start_date.getUTCMonth(), start_date.getUTCDate()+minDiff, end_time.hr, end_time.min, 0);
+            let location = course['location'][i];
+            String.prototype.newFormat = function() {
+                var args = arguments;
+                return this.replace(/{(\d+)}/g, function(match, number) {
+                    return typeof args[number] != 'undefined'
+                    ? args[number]
+                    : match
+                    ;
+                });
+            };
+            let event_name = "{0} - {1} ({2}, {3})".newFormat(classTitle, classType, classSec, classNbr);
+            let vevent = new ICAL.Component('vevent'),
+            event = new ICAL.Event(vevent);
+            event.summary = event_name;
+            event.startDate = ICAL.Time.fromJSDate(dtstart).convertToZone(timezone);
+            event.endDate = ICAL.Time.fromJSDate(dtend).convertToZone(timezone);
+            event['location'] = location;
+            let rrule = new ICAL.Recur({
+                'freq': 'WEEKLY',
+                'until': ICAL.Time.fromJSDate(end_date).convertToZone(timezone),
+                'byday': byday
             });
-        };
-        let event_name = "{0} - {1} ({2}, {3})".newFormat(classTitle, classType, classSec, classNbr);
-        let vevent = new ICAL.Component('vevent'),
-        event = new ICAL.Event(vevent);
-        event.summary = event_name;
-        event.startDate = ICAL.Time.fromJSDate(dtstart).convertToZone(timezone);
-        event.endDate = ICAL.Time.fromJSDate(dtend).convertToZone(timezone);
-        event['location'] = location;
-        let rrule = new ICAL.Recur({
-            'freq': 'WEEKLY',
-            'until': ICAL.Time.fromJSDate(end_date).convertToZone(timezone),
-            'byday': byday
-        });
-        vevent.updatePropertyWithValue('rrule', rrule);
-        if (classType != 'MID'){
-            cal.addSubcomponent(vevent);
+            vevent.updatePropertyWithValue('rrule', rrule);
+            if (classType != 'MID'){
+                cal.addSubcomponent(vevent);
+            }
         }
     }
     // let calendar_str = cal.toString();
@@ -112,7 +129,7 @@ chrome.runtime.onInstalled.addListener(function() {
       {
         conditions: [
           new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: { urlContains: 'https://rawgit.com/sidpagariya/MProjects/master/Schedule-maker/' },
+            pageUrl: { urlContains: 'https://csprod.dsc.umich.edu/psp/csprodnonop/EMPLOYEE/SA/c/SA_LEARNER_SERVICES.SSS_STUDENT_CENTER.GBL' },
           })
         ],
         actions: [ new chrome.declarativeContent.ShowPageAction() ]
--- Schedule-maker/chrome_ext/js/content.js
+++ Schedule-maker/chrome_ext/js/content.js
@@ -11,13 +11,7 @@ function convertTime(time){
     return {hr:+sHours,min:+sMinutes};
 }
 function parseSchedule(){
-    //var tbody = $('[id="STDNT_WEEK_SCHD$scroll$0"]', $('#ptifrmtgtframe')[0].contentWindow.document).children()[0];
-    var tbody;
-    if (!$('#ptifrmtgtframe')[0]){
-        tbody = $('[id="STDNT_WEEK_SCHD$scroll$0"]').children()[0];
-    } else {
-        tbody = $('[id="STDNT_WEEK_SCHD$scroll$0"]', $('#ptifrmtgtframe')[0].contentWindow.document).children()[0];
-    }
+    var tbody = $('[id="STDNT_WEEK_SCHD$scroll$0"]', $('#ptifrmtgtframe')[0].contentWindow.document).children()[0];
     var termsched = tbody.firstElementChild.firstElementChild.firstElementChild.innerText
     var tbodymain = tbody.children[1].firstElementChild.firstElementChild.firstElementChild
     var jsonObj = {};
@@ -37,19 +31,31 @@ function parseSchedule(){
         classI.nbr = course[1].split(" ")[1].slice(1, -1);
         item.course = classI;
         var sched = tbodymain.children[i].children[1+offset].firstElementChild.innerText.split('\n');
-        fullSched = {};
-        days = sched[0].split(" ")[0].split(/(?=[A-Z])/);
-        fullSched.days = days.map(function(e){return e.toUpperCase()});
-        time = {}
-        time.from = convertTime(sched[0].split(" ")[1]);
-        time.to = convertTime(sched[0].split(" ")[3]);
-        fullSched.time = time;
-        item.sched = fullSched;
-        item.location = sched[1];
+        subscheds = [];
+        locs = [];
+        for (var j=0; j<sched.length; j+=2){
+            subschedObj = {};
+            days = sched[j].split(" ")[0].split(/(?=[A-Z])/);
+            subschedObj.days = days.map(function(e){return e.toUpperCase()});
+            time = {}
+            try {
+                time.from = convertTime(sched[j].split(" ")[1]);
+                time.to = convertTime(sched[j].split(" ")[3]);
+            }
+            catch(err) {
+                // this means time is not provided
+                continue;
+            }
+            subschedObj.time = time;
+            subscheds.push(subschedObj)
+            locs.push(sched[j+1]);
+        }
+        item.sched = subscheds;
+        item.location = locs;
         schedule.push(item);
     }
     jsonObj.schedule = schedule;
-    return jsonObj;
+    createICal(jsonObj);
 }
 
 chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
